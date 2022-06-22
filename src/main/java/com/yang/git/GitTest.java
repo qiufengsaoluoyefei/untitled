36457bc5b6301f38a40d30f30654df4e2c346473
yang <yang@qq.com>
2022-06-22 20:07:02 +0800

    second commit

---
+++ src/main/java/com/yang/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yang.git;
+
+/**
+ * @author yang
+ * @create 2022-06-22 20:05
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
